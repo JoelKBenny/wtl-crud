2c9f3a3b23893a520e74fd7eab334639ded30d4f
Joel K Benny <joeljobenny@gmail.com>
2023-09-29 23:41:12 +0530

    first commit

---
+++ dotnet/resources/views/welcome.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<form, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<center>
+    <form action="{{ route('add-student') }}" method="post">
+        @csrf
+        Name <input type="text" name="name"><br>
+        <button type="submit"> Submit </button>
+    </form>
+<br>
+<table border="1">
+    <tr>
+        <td>
+            #
+        </td>
+        <td>
+            Name
+        </td>
+        <td colspan="2">
+            Action
+        </td>
+    </tr>
+    @if ($data->count()>0)
+        @foreach ($data as $key=>$value)
+            <tr>
+                <td>
+                    {{ $key++ }}
+                </td>
+                <td>
+                    {{$value->name}}
+                </td>
+                <td>
+                    <a href="{{ route('view-student',$value->id) }}">Edit </a> |<a href="{{ route('delete-student',$value->id) }}">deletegi </a>
+                </td>
+            </tr>   
+        @endforeach
+    @else
+        <tr>
+            <td> No data found </td>
+        </tr>
+    @endif
+</table>
+</center>
+</body>
+</html>
